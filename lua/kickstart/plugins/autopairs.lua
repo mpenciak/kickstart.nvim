3301c45d5d878a4007ddcfee703dc5c0ec432073
Matej Penciak <matej.penciak@gmail.com>
2024-07-15 17:32:16 -0400

    some Lean QOL

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -11,6 +11,13 @@ return {
     -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
     local cmp = require 'cmp'
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    cmp.event:on(
+      'confirm_done',
+      cmp_autopairs.on_confirm_done {
+        filetypes = {
+          lean = false,
+        },
+      }
+    )
   end,
 }
