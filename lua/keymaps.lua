71b43507b61c9f2dbdee90c0be6cc58e9c54b732
Matej Penciak <matej.penciak@gmail.com>
2024-07-15 17:33:36 -0400

    remove lvim keybinding

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,3 @@
--- Navigation keymaps
-vim.keymap.set('n', 'H', 'Hzz')
-vim.keymap.set('n', 'L', 'Lzz')
-
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
