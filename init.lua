25da93c4e0ec989b47ffd11cf9b2c523d84faef0
Matej Penciak <matej.penciak@gmail.com>
2025-01-13 13:15:19 -0500

    add octo

--- init.lua
+++ init.lua
@@ -74,6 +74,19 @@ local plugins = {
   -- diff view
   'sindrets/diffview.nvim',
 
+  -- Octo for Github integration
+  {
+    'pwntester/octo.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('octo').setup()
+    end,
+  },
+
   -- Markdown previewer
   require 'plugins.mdpreview',
 
